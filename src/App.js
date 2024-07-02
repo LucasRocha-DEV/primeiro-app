a15599c7a978cd07e3ac7f09e6566950241795ea
LucasRocha <lucaspdsr@gmail.com>
2024-07-02 09:06:23 -0300

    Adiciona campos de formulário para nome, email e idade, inclui botão de submit e exibe informações do usuário após registro.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,49 @@
 import Nome from "./components/Nome";
 
 function App() {
+  const [nome,setNome] = useState('');
+  const [email,setEmail] = useState('');
+  const [idade,setIdade] = useState(0);
+
+function handleRegister(e){
+  alert(TESTE);
+}
+
   return (
     <div className="App">
-<form>
-  <label>nome</label><br/>
-  <input placeholder="Digite seu nome"></input><br/>
+      <h1>Formulário de Cadastro</h1>
+
+<form onSubmit={handleRegister}>
+
+  <label>Nome</label><br/>
+  <input 
+  placeholder="Digite seu nome" 
+  value={nome}
+  onChange={ (e) => setNome(e.target.value) }/><br/>
   
-  <label>idade</label><br/>
-  <input placeholder="Digite sua idade"></input><br/>
+  <label>Email</label><br/>
+  <input 
+  placeholder="Digite seu email"
+  value={email}
+  onChange={ (e) => setEmail(e.target.value) }
+  ></input><br/>
+
+  <label>Idade</label><br/>
+  <input placeholder="Digite sua idade"
   
-  <label>email</label><br/>
-  <input placeholder="Digite seu email"></input><br/>
+  ></input><br/>
+  
+
 
   <button type="submit">Registrar</button>
 </form>
+    <br/><br/>
+
+    <div>
+      <span>Bem Vindo: {user.nome}</span>
+      <span>Email: {user.email}</span>
+      <span>Idade: {user.idade}</span>
+    </div>
     </div>
   );
 }

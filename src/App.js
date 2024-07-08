239341f26b78883467dd7e8d2675a0ac073869fb
LucasRocha <lucaspdsr@gmail.com>
2024-07-08 19:25:00 -0300

    feat: Adiciona campos de formulário para nome, email e idade, inclui botão de submit e exibe informações do usuário após registro.

--- src/App.js
+++ src/App.js
@@ -1,49 +1,46 @@
-import Nome from "./components/Nome";
-
+import React, { useState } from 'react';
+import './components/App.css';
 function App() {
-  const [nome,setNome] = useState('');
-  const [email,setEmail] = useState('');
-  const [idade,setIdade] = useState(0);
+  const [nome, setNome] = useState('');
+  const [email, setEmail] = useState('');
+  const [idade, setIdade] = useState('');
 
-function handleRegister(e){
-  alert(TESTE);
-}
+  function handleRegister(e) { // Função para manipular/alterar o evento de submit do formulário
+    e.preventDefault(); // Previne o comportamento padrão do formulário (recarregar a página) ao submeter o formulário
+    alert('TESTE');
+  }
 
   return (
-    <div className="App">
-      <h1>Formulário de Cadastro</h1>
-
-<form onSubmit={handleRegister}>
-
-  <label>Nome</label><br/>
-  <input 
-  placeholder="Digite seu nome" 
-  value={nome}
-  onChange={ (e) => setNome(e.target.value) }/><br/>
-  
-  <label>Email</label><br/>
-  <input 
-  placeholder="Digite seu email"
-  value={email}
-  onChange={ (e) => setEmail(e.target.value) }
-  ></input><br/>
-
-  <label>Idade</label><br/>
-  <input placeholder="Digite sua idade"
-  
-  ></input><br/>
-  
-
-
-  <button type="submit">Registrar</button>
-</form>
-    <br/><br/>
-
     <div>
-      <span>Bem Vindo: {user.nome}</span>
-      <span>Email: {user.email}</span>
-      <span>Idade: {user.idade}</span>
-    </div>
+      <form onSubmit={handleRegister}>
+        <label className="label-bold">Nome</label><br/>
+        <input 
+          placeholder="Digite seu nome" 
+          value={nome}
+          onChange={(e) => setNome(e.target.value)}
+        /><br/>
+        <label className="label-bold">Email</label><br/>
+        <input 
+          placeholder="Digite seu email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        /><br/>
+        <label className="label-bold">Idade</label><br/>
+        <input 
+          placeholder="Digite sua idade"
+          value={idade}
+          onChange={(e) => setIdade(e.target.value)}
+        /><br/>
+        <button type="submit" className="button-register">Registrar</button>
+      </form>
+
+      <br/><br/>
+
+      <div>
+        <span className="label-bold">Bem Vindo:</span> {nome}<br/>
+        <span className="label-bold">Email:</span> {email}<br/>
+        <span className="label-bold">Idade:</span> {idade}<br/>
+      </div>
     </div>
   );
 }

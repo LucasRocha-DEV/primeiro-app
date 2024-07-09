659253c0ab9cc01dedf8276733a8c05915bf8b05
LucasRocha <lucaspdsr@gmail.com>
2024-07-09 15:16:11 -0300

    feat: Add local storage functionality to save and load tasks

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import './components/App.css';
 function App() {
-  const [input, setInput] = useState(''); // Estado para armazenar o valor do input
-  const [tarefas, setTarefas] = useState([]); // Corrigido para ser uma lista
-  
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([]);
+
+  useEffect(() => {
+    const tarefasSalvas = localStorage.getItem('@tarefas');
+    console.log('Tarefas carregadas:', tarefasSalvas);
+    if (tarefasSalvas) {
+      setTarefas(JSON.parse(tarefasSalvas));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('@tarefas', JSON.stringify(tarefas));
+    console.log('Tarefas salvas:', tarefas);
+    // Removido o alert para evitar interrupções no fluxo da aplicação
+  }, [tarefas]);
+
   function handleRegister(e) { // Função para manipular/alterar o evento de submit do formulário
     e.preventDefault(); // Previne o comportamento padrão do formulário (recarregar a página) ao submeter o formulário
+    if (!input.trim()) return; // Verifica se o input é uma string vazia ou contém apenas espaços. Se sim, retorna e não faz nada.
     setTarefas([...tarefas, input]); // Adiciona a tarefa atual à lista de tarefas
     setInput(''); // Limpa o input após adicionar a tarefa
   }
